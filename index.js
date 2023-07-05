491c93348f09a4e8c3748f4c4a44667051b0a06d
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-05 12:34:13 -0600

    js into html

--- index.js
+++ index.js
@@ -26,9 +26,7 @@
 class JSXDemo extends React.Component {
     render() {
         return (<div>
-            <h1>Wrapped</h1>
-            <img src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
-
+           <h1>My number is: {2*8.4}</h1>
         </div>
         )
     }
