c2a2e5eb538d649555179e97e698d69625e9a6ba
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-05 12:21:56 -0600

    jsx

--- index.js
+++ index.js
@@ -11,14 +11,24 @@
 // }
 
 
-function Hello() {
-    return (
-        <div>
-        <h1>Hello there!</h1>
-        <h1>Hello there!</h1>
-        <h1>Hello there!</h1>
-        </div>
-    )
+// function Hello() {
+//     return (
+//         <div>
+//         <h1>Hello there!</h1>
+//         <h1>Hello there!</h1>
+//         <h1>Hello there!</h1>
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(<Hello/>, document.getElementById('root'))
+
+class JSXDemo extends React.Component {
+    render() {
+        return (
+            <img src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+        )
+    }
 }
 
-ReactDOM.render(<Hello/>, document.getElementById('root'))
+ReactDOM.render(<JSXDemo/>, document.getElementById('root'))
