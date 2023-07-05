a6d3fcb69b3d3fddc0a2f708faa340a3d82604c7
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-05 12:37:07 -0600

    funcion into html

--- index.js
+++ index.js
@@ -23,10 +23,15 @@
 
 // ReactDOM.render(<Hello/>, document.getElementById('root'))
 
+function getMood() {
+    const moods = ['angry', 'hungry', 'silly']
+    return moods[Math.floor(Math.random() * moods.length)]
+}
+
 class JSXDemo extends React.Component {
     render() {
         return (<div>
-           <h1>My number is: {2*8.4}</h1>
+           <h1>My current mood is: {getMood()}</h1>
         </div>
         )
     }
